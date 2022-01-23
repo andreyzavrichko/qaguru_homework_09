7a45a13efeb0ed2bc44845581f9eeafd1275b8b1
Andrey Zavrichko <andrey@zavrichko.dev>
2022-01-23 08:46:18 +0200

    create start project

---
+++ src/test/java/ru/zavrichko/tests/RegistrationTest.java
@@ -0,0 +1,56 @@
+package ru.zavrichko.tests;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest extends TestBase {
+
+    @Test
+    void RegistrationTest() {
+        // Open website
+        open("/automation-practice-form");
+
+        // Find Element and Set value
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Smirnov");
+        $("#userEmail").setValue("alex.smirnov@gmail.com");
+        $(".custom-control-label").click();
+        $("#userNumber").setValue("5648798798");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2014");
+        $x("//div[contains(text(),'15')]").click();
+        $("#subjectsInput").setValue("Eng").pressEnter();
+        $x("//label[contains(text(),'Sports')]").click();
+        $x("//label[contains(text(),'Reading')]").click();
+        $x("//label[contains(text(),'Music')]").click();
+        File file = new File("src/test/resources/photo.jpg");
+        Selenide.$(byId("uploadPicture")).uploadFile(file);
+        $("#currentAddress").setValue("Moscow, Manoilov Street, 64");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Gurgaon").pressEnter();
+        $("#submit").click();
+
+        // Assertion
+        $("#example-modal-sizes-title-lg").shouldBe(visible);
+        $(".table-responsive").shouldHave(text("Alex"));
+        $(".table-responsive").shouldHave(text("Smirnov"));
+        $(".table-responsive").shouldHave(text("alex.smirnov@gmail.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("5648798798"));
+        $(".table-responsive").shouldHave(text("15 May,2014"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive").shouldHave(text("photo.jpg"));
+        $(".table-responsive").shouldHave(text("NCR Gurgaon"));
+
+    }
+}
